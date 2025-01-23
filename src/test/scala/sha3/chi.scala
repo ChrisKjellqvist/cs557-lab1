7aba4aa21a3d9cf71b74e94983b23623331b602f
chriskjellqvist <chriskjellqvist@gmail.com>
2025-01-23 17:27:05 -0500

    initial

---
+++ src/test/scala/sha3/chi.scala
@@ -0,0 +1,8 @@
+package sha3
+
+import chiseltest._
+
+class ChiModuleTests extends TestWithBackendSelect with ChiselScalatestTester {
+  behavior of "ChiModule"
+  // YOUR TEST CODE BELOW
+}
---
+++ src/test/scala/sha3/rhopi.scala
@@ -0,0 +1,10 @@
+package sha3
+
+import chiseltest._
+
+class RhoPiModuleTests
+    extends TestWithBackendSelect
+    with ChiselScalatestTester {
+  behavior of "RhoPiModule"
+  // YOUR TEST CODE BELOW
+}
